6bd8fe9a3803a8a963c432ed15275d74ac81df3e
Nazwa Oktaviani <nazwaoktaviani58@gmail.com>
2026-02-24 15:28:03 +0700

    updatelagii

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ import express from 'express';
 
 const app = express();
 
+// global middleware
 app.use((req, res, next) => {
   res.locals.msg = `Kau ke sini dulu ${req.path}`;
   next();
 });
 
 app.get('/', (req, res) => {
-  res.send(res.locals.msg + 'Halooo');
+  res.send(res.locals.msg + ' Halooo');
 });
 
 app.get('/say/:greeting', (req, res) => {
   const { greeting } = req.params;
   res.send(greeting);
+});
+
+// route baru 1: static route
+app.get('/about', (req, res) => {
+  res.send(res.locals.msg + ' Ini halaman about');
+});
+
+// route baru 2: dynamic route
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(res.locals.msg + ` Hello ${name}`);
 });
 
 app.listen(3000);
