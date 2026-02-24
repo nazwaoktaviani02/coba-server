a88b64568f085f6e9a29a36eb699b87ad69563de
Nazwa Oktaviani <nazwaoktaviani58@gmail.com>
2026-02-24 14:13:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+  if (true) {
+    next(new Error('Not Authorized'));
+    return;
+  }
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.send('Error Occurred');
+});
+
+app.get('/', (req, res) => {
+  res.send(' - Helloww');
+});
+
+app.listen(3000);
