735226f44559c581e70ce4e86fb4bd5f634473e2
Nazwa Oktaviani <nazwaoktaviani58@gmail.com>
2026-02-27 11:51:56 +0700

    cobaupdatecorslagi

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 import noteRouter from './routes/notes.js';
 import cors from "cors";
 
-
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 // mongoose.connect(
 //   "mongodb://user01:admin123@ac-yn1f84i-shard-00-00.desnfid.mongodb.net:27017,ac-yn1f84i-shard-00-01.desnfid.mongodb.net:27017,ac-yn1f84i-shard-00-02.desnfid.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-yn1f84i-shard-0&authSource=admin&retryWrites=true&w=majority"
