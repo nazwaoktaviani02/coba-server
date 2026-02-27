41c1f076cf6a404976aec011df3f23544e6d7c3f
Nazwa Oktaviani <nazwaoktaviani58@gmail.com>
2026-02-27 11:50:16 +0700

    updateimportcors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from "mongoose";
 import noteRouter from './routes/notes.js';
-// import cors from "cors";
+import cors from "cors";
+
+
+app.use(cors());
 
 // mongoose.connect(
 //   "mongodb://user01:admin123@ac-yn1f84i-shard-00-00.desnfid.mongodb.net:27017,ac-yn1f84i-shard-00-01.desnfid.mongodb.net:27017,ac-yn1f84i-shard-00-02.desnfid.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-yn1f84i-shard-0&authSource=admin&retryWrites=true&w=majority"
